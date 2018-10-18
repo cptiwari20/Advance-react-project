0402e4017611d6ed62e775b42c42732b10bb735a
cptiwari20 <cptiwari20@gmail.com>
2018-10-18 19:38:21 +0530

    initial commit

---
+++ src/components/CommentBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class CommentBox extends Component {
+  render(){
+    return(
+      <div>
+        This is a starting of the comment Box.
+        <textarea>Hahahah</textarea>
+      </div>
+    )
+  }
+}
+export default CommentBox;
