6bee1f16c1da91123f289f36b0b9b3d6e8a5714e
cptiwari20 <cptiwari20@gmail.com>
2018-10-30 19:25:28 +0530

    added some elements to commentBox Component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 
 class CommentBox extends Component {
+  state = { comment: ''};
+
+  handleChange(event) {
+    this.setState({comment: event.target.value });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // call an action creator to add a comment to a list
+
+    this.setState({comment: 'Submitted'})
+  }
   render(){
     return(
-      <div>
-        Comment Box
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <h2>Comment Here</h2>
+        <textarea 
+          value={this.state.comment}
+          onChange={this.handleChange} 
+        />
+        <button>Submit Comment</button>
+      </form>
     )
   }
 }
