210236c23dd6e5d049f6e32ba97eeb983a383438
cptiwari20 <cptiwari20@gmail.com>
2018-10-31 21:12:05 +0530

    added empty testarea test for comment box

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -23,3 +23,16 @@ it('has a text area where user can type in', () => {
   wrapped.update();
   expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
 });
+
+it('has an empty text area after submit', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment'}
+  });
+  wrapped.update();
+
+  wrapped.find('form').simulate('submit');
+  wrapped.update();
+ 
+  expect(wrapped.find('textarea').prop('value')).toEqual('');
+});
+
