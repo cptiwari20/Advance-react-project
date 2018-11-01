5572e673ace770bb1642cf3243d4c73529f08567
cptiwari20 <cptiwari20@gmail.com>
2018-11-01 21:27:35 +0530

    added first test for the component have redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { commentReducer } from 'reducers/commentReducer';
+import commentReducer from 'reducers/comments';
 
 export default combineReducers({
   comments: commentReducer
