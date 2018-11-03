6a865fb550cfd24f8be9a8b307e479dd33e37a4a
cptiwari20 <cptiwari20@gmail.com>
2018-11-03 21:47:59 +0530

    added redux side auth sample and some more JSX code

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,4 @@
 export const SAVE_COMMENT = 'save_comment';
 export const FETCH_COMMENT = 'fetch_comment';
+export const CHANGE_AUTH = 'change-auth';
+
--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,50 @@
 import React, { Component  } from "react";
-import { Route } from 'react-router-dom';
-
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import * as actions from "actions";
 
 class App extends Component {
+
+  renderButtons(){
+    if(this.props.auth){
+      return <button onClick={() => this.props.changeAuth(false)}>
+        Sign Out
+        </button>
+    }else{
+      return <button onClick={() => this.props.changeAuth(true)}>
+        Sign In
+      </button>
+    }
+  }
+
+  renderHeader(){
+    return (
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/post'>Post a Comment</Link>
+        </li>
+        <li>
+          {this.renderButtons()}
+        </li>  
+      </ul>
+    )
+  }
   render(){
     return(
       <div>
+        {this.renderHeader()}
         <Route path='/post' component={CommentBox} />
         <Route path='/' exact component={CommentList} />
       </div>
     )
   }
 };
-
-export default App;
+function mapStateToProps (state){
+  return { auth: state.auth }
+}
+export default connect(mapStateToProps, actions)(App);
